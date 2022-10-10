03ab8815f15953a2cfeda51b11de93bd501e6abc
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-10-10 15:53:21 +0200

    Update

--- src/js/api/posts/getPost.mjs
+++ src/js/api/posts/getPost.mjs
@@ -1,23 +1,14 @@
 import { api_social_url } from "../constants.mjs";
 import { fetchWithAuth } from "../fetchWithToken.mjs";
 
-const endpoint = "/posts?_author=true&_comments=true&reactions=true";
+const endpoint = "/posts";
 
 export async function getPost(id) {
   if (!id) {
     throw new Error("Get requires a postID");
   }
-  const getPostUrl = `${api_social_url}${endpoint}/${id}`;
+  const getPostUrl = `${api_social_url}${endpoint}/${id}?_author=true&_comments=true&reactions=true`;
 
   const response = await fetchWithAuth(getPostUrl);
-
-  console.log(response.json());
   return await response.json();
 }
-
-/*import { getPosts } from "./getPosts.mjs";
-
-export async function getPost(id) {
-  const posts = await getPosts();
-  return posts.find((post) => post.id === id);
-}*/
--- src/js/api/posts/getPosts.mjs
+++ src/js/api/posts/getPosts.mjs
@@ -1,7 +1,7 @@
 import { api_social_url } from "../constants.mjs";
 import { fetchWithAuth } from "../fetchWithToken.mjs";
 
-const endpoint = "/posts?_author=true&_comments=true&reactions=true";
+const endpoint = "/posts?_author=true&_comments=true&reactions=true&limit=300";
 
 export async function getPosts() {
   const getPostsUrl = `${api_social_url}${endpoint}`;
