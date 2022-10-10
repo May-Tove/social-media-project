08dae32a765fc499e38472139bda411948aeb8b9
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-10-10 15:53:13 +0200

    Adding more error messages

--- src/js/components/error.js
+++ src/js/components/error.js
@@ -12,3 +12,13 @@ export function displayError(message = "Unknown error") {
   <h6 class="mb-0 text-white">${message}</h6>
   </div>`;
 }
+
+export function noResultError(message = "No result") {
+  return ` <div class="p-4 flex-column justify-content-center align-items-center mt-3">
+  <h6 class="mb-0 text-muted text-center">${message}</h6>
+  </div>`;
+}
+
+export function authError(message) {
+  return `<div class='alert alert-danger p-2' role="alert">${message}</div>`;
+}
