a5edfd512840e2d2dc26fa4510461d09ab5d63ba
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-10-10 15:52:58 +0200

    Updating handlers

--- src/js/handlers/createPost.mjs
+++ src/js/handlers/createPost.mjs
@@ -1,4 +1,5 @@
 import { createPost } from "../api/posts/index.mjs";
+
 export function createPostFormListener() {
   const form = document.querySelector("#createForm");
 
@@ -6,13 +7,11 @@ export function createPostFormListener() {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
 
+      const form = e.target;
       const formData = new FormData(form);
       const post = Object.fromEntries(formData.entries());
 
       createPost(post);
-      console.log(post);
-      window.location.reload();
-      form.reset();
     });
   }
 }
--- src/js/handlers/index.mjs
+++ src/js/handlers/index.mjs
@@ -3,3 +3,6 @@ export * from "./login.mjs";
 export * from "./createPost.mjs";
 export * from "./updatePost.mjs";
 export * from "./updateProfile.mjs";
+export * from "./removePost.mjs";
+export * from "./search.mjs";
+export * from "./filter.mjs";
