41ebc940225e1a747f5207bcfd423684af34fca8
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-01-04 17:10:47 +0100

    update

--- src/js/api/posts/getPosts.mjs
+++ src/js/api/posts/getPosts.mjs
@@ -1,7 +1,7 @@
 import { api_social_url } from "../constants.mjs";
 import { fetchWithAuth } from "../fetchWithToken.mjs";
 
-const endpoint = "/posts?_author=true&_comments=true&reactions=true&limit=200";
+const endpoint = "/posts?_author=true&_comments=true&reactions=true";
 
 /**
  * Get all posts from API using GET method(default)
