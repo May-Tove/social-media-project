bc679c770185de4be8c661ff180719e5d074bd71
May-Tove <89157761+May-Tove@users.noreply.github.com>
2023-01-04 17:09:23 +0100

    Add a router and reduce functions being called

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,31 @@ import * as render from "./render/index.mjs";
 import * as listeners from "./handlers/index.mjs";
 
 render.getUserHeaderDetails();
-render.renderProfile();
-render.renderFormAvatar();
-render.renderPostHtml();
-render.renderPostsHtml();
 
-listeners.loginFormListener();
-listeners.registerFormListener();
-listeners.createPostFormListener();
-listeners.updatePostListener();
-listeners.removePostListener();
-listeners.updateProfileListener();
+const path = location.pathname;
+
+function router() {
+  switch (path) {
+    case "/src/pages/posts/feed/index.html":
+      render.renderPostsHtml();
+      listeners.createPostFormListener();
+      render.renderFormAvatar();
+      break;
+    case "/src/pages/posts/post/index.html":
+      render.renderPostHtml();
+      break;
+    case "/src/pages/profile/login/index.html":
+      listeners.loginFormListener();
+      break;
+    case "/src/pages/profile/register/index.html":
+      listeners.registerFormListener();
+      break;
+    case "/src/pages/profile/index.html":
+      render.renderProfile();
+      render.renderFormAvatar();
+      listeners.createPostFormListener();
+      break;
+  }
+}
+
+router();
